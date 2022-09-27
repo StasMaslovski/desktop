73f858b5aebe5bcafa7d506362a9d27b35b564f7
StasMaslouski <msv262626@gmail.com>
2022-09-27 17:50:01 +0400

    add hook for allure report generating

--- conftest.py
+++ conftest.py
@@ -1,14 +1,46 @@
 import pytest
 from appium import webdriver
 from page_objects.base_page import BasePageObject
+import allure
+import os
 
 
 @pytest.fixture(scope='function')
 def driver():
-    desired_capabilities = {"app": "C:\\Users\\anduser\\AppData\\Local\\Reverso\\Reverso\\Reverso.exe"}
+    desired_capabilities = {
+        "app": "C:\\Users\\anduser\\AppData\\Local\\Reverso\\Reverso\\Reverso.exe"}  # path to application ".exe" file
     driver = webdriver.Remote(
         command_executor='http://127.0.0.1:4723/wd/hub',
         desired_capabilities=desired_capabilities)
     yield driver
     driver.close_app()
     BasePageObject(driver).accept_alert()
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item):
+    """
+         get the hook function for the state of each use case
+        :param item:  test case
+        :param call:  test procedure
+        :return:
+        """
+    #  get the result of the call to the hook method
+    outcome = yield
+    rep = outcome.get_result()  # get the test report from the result of the call to the hook method
+    # rep.when represents the test step and only gets the use case call  the result of the execution is a failure.
+    if rep.when == 'call' and rep.failed:
+        mode = 'a' if os.path.exists('failures') else 'w'
+        try:
+            with open('failures', mode):
+                if 'driver' in item.fixturenames:
+                    web_driver = item.funcargs['driver']
+                else:
+                    return
+            allure.attach(
+                web_driver.get_screenshot_as_png(),  # add screenshot of allure report
+                name='screenshot',
+                attachment_type=allure.attachment_type.PNG
+            )
+        except Exception as e:
+            print('Fail to take screen-shot: {}'.format(e))
--- tests/reverso_context_test.py
+++ tests/reverso_context_test.py
@@ -1,5 +1,5 @@
 import pytest
-
+import allure
 from page_objects.translate_text_page import TranslateText
 
 
@@ -13,7 +13,11 @@ from page_objects.translate_text_page import TranslateText
 )
 def test_translate_from_rus_to_eng(driver, russian_word, english_word):
     translate_text_page = TranslateText(driver)
-    translate_text_page._fill_input(translate_text_page.text_translate_field_locator, russian_word)
-    translate_text_page._tap_enter(translate_text_page.text_translate_field_locator)
-    translate_text_page._clear_field(translate_text_page.text_translate_field_locator)
-    assert translate_text_page.is_element_with_text_present_on_the_screen(english_word)
+    with allure.step(f"Input {russian_word} in the text field"):
+        translate_text_page._fill_input(translate_text_page.text_translate_field_locator, russian_word)
+    with allure.step("Taping the enter button"):
+        translate_text_page._tap_enter(translate_text_page.text_translate_field_locator)
+    with allure.step("Clearing the text field"):
+        translate_text_page._clear_field(translate_text_page.text_translate_field_locator)
+    with allure.step(f"Check if {english_word} present on the screen"):
+        assert translate_text_page.is_element_with_text_present_on_the_screen(english_word)
